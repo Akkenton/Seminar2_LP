fff852ead627f0edf7aac71bac08ed79ea590f79
Akkenton <akkenton.msk@gmail.com>
2022-11-24 20:49:15 +0300

    Хакнутое решение задачи №2

--- task002/Program.cs
+++ task002/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int numberOne = number / 100;
+int numberThree = number % 10;
+
+Console.Write(numberOne);
+Console.Write(numberThree);
