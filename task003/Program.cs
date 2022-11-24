a5115c18e1056de7c5438620609780a7d9605b3a
Akkenton <akkenton.msk@gmail.com>
2022-11-24 21:33:09 +0300

    Решение задачи №3

--- task003/Program.cs
+++ task003/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+
+// здесь мы ставим условие выбора большего числа как кратное
+
+if (num2 > num1) // если искомое кратное это второе число
+{
+    if (num2 % num1 != 0)
+    {
+        Console.WriteLine($"Число {num2} не кратно {num1}! Остаток от деления: {num2 % num1}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num2} кратно {num1}");
+    }
+}
+else // если искомое кратное это иные варианты
+{
+    {
+        if (num1 % num2 != 0)
+        {
+            Console.WriteLine($"Число {num1} не кратно {num2}! Остаток от деления: {num1 % num2}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {num1} кратно {num2}");
+        }
+    }
+}
