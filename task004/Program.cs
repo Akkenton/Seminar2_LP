c2613ab553dcfa708ba71019162cffe874ae5d89
Akkenton <akkenton.msk@gmail.com>
2022-11-24 21:48:47 +0300

    Решение задачи №4

--- task004/Program.cs
+++ task004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Clear();
+Console.WriteLine("Введите число для проверки");
+int number = int.Parse(Console.ReadLine());
+if ((number % 7 == 0) && (number % 23 == 0))
+{
+    Console.WriteLine($"Кратно одновременно!");
+}
+else
+{
+    Console.WriteLine($"Не кратно одновременно!");
+}
