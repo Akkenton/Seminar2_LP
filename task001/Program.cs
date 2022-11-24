aa000ad05082714465c2a37bc8ff1d7ba1a1d057
Akkenton <akkenton.msk@gmail.com>
2022-11-24 20:34:31 +0300

    Добавлено решение задачи task001 и файл readme.md

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и
+// показывает наибольшую цифру числа.
+
+
+Console.Clear();
+int num = new Random().Next(10, 99);
+Console.WriteLine($"Случайное число: {num}");
+int secVal = num % 10;
+int firstVal = num / 10;
+if (secVal > firstVal)
+{
+    Console.WriteLine($"Большая цифра в двузначном числе: {secVal}");
+}
+if (secVal < firstVal)
+{
+    Console.WriteLine($"Большая цифра в двузначном числе: {firstVal}");
+}
+if (secVal == firstVal)
+{
+    Console.WriteLine($"Большая цифра в двузначном числе: {firstVal}");
+}
